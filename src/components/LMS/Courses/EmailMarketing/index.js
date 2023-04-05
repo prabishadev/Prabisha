98b405fcbf91c7631a7020da20c32bc6cd471611
saicharan20012007 <saicharan20012007@gmail.com>
2023-04-06 02:52:26 +0530

    added some digial marketing pdfs and mcqs

---
+++ src/components/LMS/Courses/EmailMarketing/index.js
@@ -0,0 +1,320 @@
+import React, { useState } from "react";
+import Header from "../../Header";
+
+import {
+  CourseContentContainer,
+  CourseImage,
+  ConceptTitle,
+  ConceptDescription,
+  DownloadCertificate,
+  BtnContainer,
+  CountContainer,
+  Score,
+  QNSCount,
+  LinkItem,
+  ExamPassed,
+  Option,
+  StartExam,
+  Certificate,
+  FailedMessage,
+  ExamContainer,
+  Question,
+  ExamTitle,
+} from "./EMMStyledComponents";
+
+const questions = [  {    question: "What is the purpose of an email opt-in?",    options: [      "To verify a user's email address",      "To send promotional emails without consent",      "To give users the option to receive emails from a company",      "To ensure that email recipients are over 18 years old"    ],
+    answer: "To give users the option to receive emails from a company"
+  },
+  {
+    question: "What is the most effective way to grow your email list?",
+    options: [
+      "Purchasing an email list",
+      "Running a contest or giveaway",
+      "Sending unsolicited emails",
+      "Posting on social media"
+    ],
+    answer: "Running a contest or giveaway"
+  },
+  {
+    question: "What is the average email open rate?",
+    options: [
+      "10%",
+      "25%",
+      "50%",
+      "75%"
+    ],
+    answer: "25%"
+  },
+  {
+    question: "What is a common reason for emails to end up in spam folders?",
+    options: [
+      "Using too many exclamation marks",
+      "Sending too many emails to the same recipient",
+      "Including too many images or attachments",
+      "All of the above"
+    ],
+    answer: "All of the above"
+  },
+  {
+    question: "What is the purpose of A/B testing in email marketing?",
+    options: [
+      "To determine the best day of the week to send emails",
+      "To compare the effectiveness of two different email campaigns",
+      "To test different subject lines for an email",
+      "To verify that email addresses are valid"
+    ],
+    answer: "To compare the effectiveness of two different email campaigns"
+  },
+  {
+    question: "What is the purpose of a call-to-action (CTA) in an email?",
+    options: [
+      "To provide additional information about a product or service",
+      "To encourage the recipient to take a specific action",
+      "To greet the recipient and introduce the sender",
+      "To apologize for any inconvenience caused"
+    ],
+    answer: "To encourage the recipient to take a specific action"
+  },
+  {
+    question: "What is the recommended frequency for sending marketing emails?",
+    options: [
+      "Once a month",
+      "Once a week",
+      "Twice a week",
+      "Every day"
+    ],
+    answer: "Once a week"
+  },
+  {
+    question: "What is a common metric used to measure email engagement?",
+    options: [
+      "Conversion rate",
+      "Cost per click",
+      "Impressions",
+      "Click-through rate"
+    ],
+    answer: "Click-through rate"
+  },
+  {
+    question: "What is the purpose of a welcome email?",
+    options: [
+      "To introduce a new product or service",
+      "To apologize for any inconvenience caused",
+      "To welcome a new subscriber and set expectations for future emails",
+      "To confirm a user's email address"
+    ],
+    answer: "To welcome a new subscriber and set expectations for future emails"
+  },
+  {
+    question: "What is the recommended length for an email subject line?",
+    options: [
+      "10-20 characters",
+      "30-40 characters",
+      "50-60 characters",
+      "70-80 characters"
+    ],
+    answer: "50-60 characters"
+  },
+  {
+    question: "What is the purpose of email segmentation?",
+    options: [
+      "To send the same message to all subscribers",
+      "To customize email content based on subscriber behavior or demographics",
+      "To ensure that emails are delivered to the inbox instead of the spam folder",
+      "To track the effectiveness of email campaigns"
+      ],
+      answer: "To customize email content based on subscriber behavior or demographics"
+      },
+      {
+      question: "What is the recommended font size for emails?",
+      options: [
+      "8-10px",
+      "10-12px",
+      "12-14px",
+      "14-16px"
+      ],
+      answer: "14-16px"
+      },
+      {
+      question: "What is the purpose of a re-engagement email?",
+      options: [
+      "To introduce a new product or service",
+      "To apologize for any inconvenience caused",
+      "To remind inactive subscribers about the benefits of being on the email list",
+      "To confirm a user's email address"
+      ],
+      answer: "To remind inactive subscribers about the benefits of being on the email list"
+      },
+      {
+      question: "What is a common metric used to measure email deliverability?",
+      options: [
+      "Conversion rate",
+      "Cost per click",
+      "Open rate",
+      "Bounce rate"
+      ],
+      answer: "Bounce rate"
+      },
+      {
+      question: "What is the purpose of a plain text version of an email?",
+      options: [
+      "To ensure that the email is accessible to all recipients, including those with visual impairments",
+      "To provide additional information about a product or service",
+      "To include images or videos that may not display in the HTML version",
+      "To confirm a user's email address"
+      ],
+      answer: "To ensure that the email is accessible to all recipients, including those with visual impairments"
+      },
+      {
+      question: "What is the recommended image-to-text ratio for emails?",
+      options: [
+      "50% text, 50% images",
+      "60% text, 40% images",
+      "70% text, 30% images",
+      "80% text, 20% images"
+      ],
+      answer: "60% text, 40% images"
+      },
+      {
+      question: "What is the purpose of email automation?",
+      options: [
+      "To send emails manually to each subscriber",
+      "To send the same message to all subscribers",
+      "To automate the process of sending targeted, personalized emails",
+      "To ensure that emails are delivered to the inbox instead of the spam folder"
+      ],
+      answer: "To automate the process of sending targeted, personalized emails"
+      },
+      {
+      question: "What is a common reason for subscribers to unsubscribe from an email list?",
+      options: [
+      "Too many emails",
+      "Irrelevant content",
+      "Poor email design",
+      "All of the above"
+      ],
+      answer: "All of the above"
+      },
+      {
+      question: "What is the purpose of email personalization?",
+      options: [
+      "To make the email look more visually appealing",
+      "To customize email content based on subscriber behavior or demographics",
+      "To ensure that emails are delivered to the inbox instead of the spam folder",
+      "To track the effectiveness of email campaigns"
+      ],
+      answer: "To customize email content based on subscriber behavior or demographics"
+      },
+      {
+      question: "What is the recommended time of day to send marketing emails?",
+      options: [
+      "6-8am",
+      "8-10am",
+      "12-2pm",
+      "2-4pm"
+      ],
+      answer: "8-10am"
+      }
+      ];
+
+
+const EmailMarketing = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showCertificate, setShowCertificate] = useState(false);
+  const [showDocument, setShowDocument] = useState(true);
+  const [showExam, setShowExam] = useState(false);
+  const [showFailed, setExamStatus] = useState("");
+
+  const handleStartExam = () => {
+    setShowDocument(false);
+    setShowExam(true);
+  };
+
+  const handleAnswer = (answer) => {
+    const isCorrect = answer === questions[currentQuestion].answer;
+    if (isCorrect) {
+      setScore(score + 5);
+    }
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      if (score >= 80) {
+        setShowCertificate(true);
+      } else {
+        setExamStatus("You have failed the Exam! Try again after some time");
+      }
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <Header />
+      <CourseContentContainer>
+        {showDocument && (
+          <>
+            <CourseImage
+              src="https://www.quest.net.nz/wp-content/uploads/Is-email-marketing-dead--1029x579.gif"
+              alt="RWD"
+            />
+
+            <ConceptTitle>Email Marketing</ConceptTitle>
+            <ConceptDescription>
+              <embed
+                src="https://www.bgateway.com/assets/templates/Email_Marketing.pdf"
+                type="application/pdf"
+                width="100%"
+                height="600px"
+              />
+            </ConceptDescription>
+            <BtnContainer>
+              <StartExam onClick={handleStartExam}>Start Exam</StartExam>
+            </BtnContainer>
+          </>
+        )}
+        {showExam && (
+          <ExamContainer>
+            <ExamTitle>Welcome to the Exam!</ExamTitle>
+            <CountContainer>
+              <Score> Score : {score}</Score>
+              <QNSCount>Question: {currentQuestion + 1}/20</QNSCount>
+            </CountContainer>
+
+            <Question>
+              {currentQuestion + 1}. {questions[currentQuestion].question}
+            </Question>
+            {questions[currentQuestion].options.map((option) => (
+              <Option key={option} onClick={() => handleAnswer(option)}>
+                {option}
+              </Option>
+            ))}
+            {showCertificate && (
+              <div>
+                <ExamPassed>Congratulations, you passed the exam!</ExamPassed>
+                <DownloadCertificate>
+                  Download your certificate:
+                </DownloadCertificate>
+                <a
+                  href="https://images.besttemplates.com/3698/Custom-made-Certificate-Design-for-Completion-of-Course.jpg"
+                  download
+                >
+                  <Certificate
+                    src="https://images.besttemplates.com/3698/Custom-made-Certificate-Design-for-Completion-of-Course.jpg"
+                    alt="Certificate"
+                  />
+                </a>
+              </div>
+            )}
+            <FailedMessage>{showFailed}</FailedMessage>
+            <StartExam>
+              <LinkItem to="/lms-available">Back</LinkItem>
+            </StartExam>
+          </ExamContainer>
+        )}
+      </CourseContentContainer>
+    </React.Fragment>
+  );
+};
+
+export default EmailMarketing;
---
+++ src/components/LMS/Courses/SocialMediaMarketing/index.js
@@ -0,0 +1,329 @@
+import React, { useState } from "react";
+import Header from "../../Header";
+
+import {
+  CourseContentContainer,
+  CourseImage,
+  ConceptTitle,
+  ConceptDescription,
+  DownloadCertificate,
+  BtnContainer,
+  CountContainer,
+  Score,
+  QNSCount,
+  LinkItem,
+  ExamPassed,
+  Option,
+  StartExam,
+  Certificate,
+  FailedMessage,
+  ExamContainer,
+  Question,
+  ExamTitle,
+} from "./SMMStyledComponents";
+
+const questions = [  {    question: "What is the main goal of social media marketing?",    options: [      "To generate leads",      "To increase website traffic",      "To promote brand awareness",      "To sell products directly to customers"    ],
+    answer: "To promote brand awareness"
+  },
+  {
+    question: "What is a social media algorithm?",
+    options: [
+      "A set of rules that determines how content is ranked and displayed on social media platforms",
+      "A tool for creating social media posts",
+      "A system for scheduling social media posts",
+      "A method for measuring social media engagement"
+    ],
+    answer: "A set of rules that determines how content is ranked and displayed on social media platforms"
+  },
+  {
+    question: "What is the best way to determine which social media platforms to use for your business?",
+    options: [
+      "Choose the platforms that your competitors are using",
+      "Use every platform available to maximize reach",
+      "Select platforms based on where your target audience spends their time",
+      "Choose the platform that you are most comfortable using"
+    ],
+    answer: "Select platforms based on where your target audience spends their time"
+  },
+  {
+    question: "What is the ideal length for a social media post?",
+    options: [
+      "Less than 50 characters",
+      "Between 50 and 100 characters",
+      "Between 100 and 150 characters",
+      "Between 150 and 200 characters"
+    ],
+    answer: "Between 100 and 150 characters"
+  },
+  {
+    question: "What is a social media influencer?",
+    options: [
+      "A person with a large following on social media who can promote products and services to their audience",
+      "A paid advertising platform on social media",
+      "A tool for scheduling social media posts",
+      "A method for measuring social media engagement"
+    ],
+    answer: "A person with a large following on social media who can promote products and services to their audience"
+  },
+  {
+    question: "What is the best way to engage with your audience on social media?",
+    options: [
+      "Post updates regularly, but do not respond to comments or messages",
+      "Only post promotional content to encourage sales",
+      "Respond promptly to comments and messages, and participate in conversations",
+      "Ignore negative comments and reviews"
+    ],
+    answer: "Respond promptly to comments and messages, and participate in conversations"
+  },
+  {
+    question: "What is a social media listening tool?",
+    options: [
+      "A tool for scheduling social media posts",
+      "A method for measuring social media engagement",
+      "A platform for creating social media ads",
+      "A tool for monitoring social media for brand mentions and customer feedback"
+    ],
+    answer: "A tool for monitoring social media for brand mentions and customer feedback"
+  },
+  {
+    question: "What is a social media hashtag?",
+    options: [
+      "A paid advertising platform on social media",
+      "A tool for scheduling social media posts",
+      "A method for measuring social media engagement",
+      "A word or phrase preceded by the # symbol used to categorize and organize social media content"
+    ],
+    answer: "A word or phrase preceded by the # symbol used to categorize and organize social media content"
+  },
+  {
+    question: "What is the ideal number of hashtags to use on Instagram?",
+    options: [
+      "1-2 hashtags",
+      "3-4 hashtags",
+      "5-6 hashtags",
+      "7-8 hashtags"
+    ],
+    answer: "5-6 hashtags"},
+    {
+    question: "What is the best way to measure the success of a social media campaign?",
+    options: [
+    "Number of followers gained",
+    "Number of likes and comments",
+    "Number of clicks and conversions",
+    "All of the above"
+    ],
+    answer: "Number of clicks and conversions"
+    },
+    {
+    question: "What is a social media crisis?",
+    options: [
+    "A situation where a company's social media account is hacked",
+    "A situation where a company's social media post receives negative feedback",
+    "A situation where a company's social media account is deleted",
+    "A situation where a company's social media account is suspended"
+    ],
+    answer: "A situation where a company's social media post receives negative feedback"
+    },
+    {
+    question: "What is the best way to handle a social media crisis?",
+    options: [
+    "Delete the offending post and pretend it never happened",
+    "Ignore the negative feedback and hope it goes away",
+    "Respond promptly and professionally, and offer a solution or apology",
+    "Blame the situation on someone else"
+    ],
+    answer: "Respond promptly and professionally, and offer a solution or apology"
+    },
+    {
+    question: "What is the best way to use social media for customer service?",
+    options: [
+    "Direct customers to a phone number or email address for support",
+    "Respond promptly to messages and comments, and offer assistance or solutions",
+    "Ignore customer complaints on social media",
+    "Use automated chatbots to handle customer service inquiries"
+    ],
+    answer: "Respond promptly to messages and comments, and offer assistance or solutions"
+    },
+    {
+    question: "What is the best way to build relationships with social media followers?",
+    options: [
+    "Only post promotional content",
+    "Ignore comments and messages",
+    "Respond promptly and personally to comments and messages, and participate in conversations",
+    "Delete negative comments and reviews"
+    ],
+    answer: "Respond promptly and personally to comments and messages, and participate in conversations"
+    },
+    {
+    question: "What is the difference between organic and paid social media reach?",
+    options: [
+    "Organic reach refers to the number of people who see your content without any advertising, while paid reach refers to the number of people who see your content as a result of paid advertising",
+    "Organic reach refers to the number of people who see your content as a result of paid advertising, while paid reach refers to the number of people who see your content without any advertising",
+    "Organic reach refers to the number of people who interact with your content, while paid reach refers to the number of people who see your content",
+    "Organic reach refers to the number of people who like and share your content, while paid reach refers to the number of people who comment on your content"
+    ],
+    answer: "Organic reach refers to the number of people who see your content without any advertising, while paid reach refers to the number of people who see your content as a result of paid advertising"
+    },
+    {
+        question: "What is the best way to work with social media influencers?",
+        options: [
+        "Send them free products and hope they promote them on social media",
+        "Pay them to promote your products or services on social media",
+        "Ignore them and focus on traditional advertising methods",
+        "Ask them to promote your products or services without offering any compensation"
+        ],
+        answer: "Pay them to promote your products or services on social media"
+        },
+        {
+        question: "What is a social media algorithm?",
+        options: [
+        "A set of rules that determines what content is shown to users on their social media feeds",
+        "A tool for tracking social media metrics and analytics",
+        "A type of social media post that is only visible to a select group of followers",
+        "A program for scheduling social media posts in advance"
+        ],
+        answer: "A set of rules that determines what content is shown to users on their social media feeds"
+        },
+        {
+        question: "What is the best time to post on social media?",
+        options: [
+        "During peak business hours on weekdays",
+        "Late at night on weekends",
+        "In the early morning on weekdays",
+        "During off-hours on weekends"
+        ],
+        answer: "During peak business hours on weekdays"
+        },
+        {
+        question: "What is the difference between social media monitoring and social media listening?",
+        options: [
+        "Social media monitoring involves tracking social media mentions of your brand or keywords, while social media listening involves analyzing and understanding the sentiment behind those mentions",
+        "Social media monitoring involves tracking social media metrics and analytics, while social media listening involves tracking social media mentions of your brand or keywords",
+        "Social media monitoring involves responding to customer complaints on social media, while social media listening involves posting promotional content on social media",
+        "Social media monitoring involves tracking your competitors' social media accounts, while social media listening involves tracking your own social media accounts"
+        ],
+        answer: "Social media monitoring involves tracking social media mentions of your brand or keywords, while social media listening involves analyzing and understanding the sentiment behind those mentions"
+        },
+        {
+        question: "What is a social media analytics tool?",
+        options: [
+        "A program for scheduling social media posts in advance",
+        "A tool for tracking social media metrics and analytics",
+        "A type of social media post that is only visible to a select group of followers",
+        "A program for creating social media graphics and images"
+        ],
+        answer: "A tool for tracking social media metrics and analytics"
+        },
+        {
+            question: "What is a social media crisis?",
+            options: [
+            "A situation where a company's social media account is hacked",
+            "A situation where a company's social media post receives negative feedback",
+            "A situation where a company's social media account is deleted",
+            "A situation where a company's social media account is suspended"
+            ],
+            answer: "A situation where a company's social media post receives negative feedback"
+            }]
+   
+
+
+const SocialMediaMarketing = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showCertificate, setShowCertificate] = useState(false);
+  const [showDocument, setShowDocument] = useState(true);
+  const [showExam, setShowExam] = useState(false);
+  const [showFailed, setExamStatus] = useState("");
+
+  const handleStartExam = () => {
+    setShowDocument(false);
+    setShowExam(true);
+  };
+
+  const handleAnswer = (answer) => {
+    const isCorrect = answer === questions[currentQuestion].answer;
+    if (isCorrect) {
+      setScore(score + 5);
+    }
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      if (score >= 80) {
+        setShowCertificate(true);
+      } else {
+        setExamStatus("You have failed the Exam! Try again after some time");
+      }
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <Header />
+      <CourseContentContainer>
+        {showDocument && (
+          <>
+            <CourseImage
+              src="https://www.aspiringteam.com/wp-content/uploads/2014/09/Social-media-marketing-banner-copy.jpg"
+              alt="RWD"
+            />
+
+            <ConceptTitle>Social Media Marketing</ConceptTitle>
+            <ConceptDescription>
+              <embed
+                src="http://www.gov.pe.ca/photos/original/IPEI_ebiz_smmkt.pdf"
+                type="application/pdf"
+                width="100%"
+                height="600px"
+              />
+            </ConceptDescription>
+            <BtnContainer>
+              <StartExam onClick={handleStartExam}>Start Exam</StartExam>
+            </BtnContainer>
+          </>
+        )}
+        {showExam && (
+          <ExamContainer>
+            <ExamTitle>Welcome to the Exam!</ExamTitle>
+            <CountContainer>
+              <Score> Score : {score}</Score>
+              <QNSCount>Question: {currentQuestion + 1}/20</QNSCount>
+            </CountContainer>
+
+            <Question>
+              {currentQuestion + 1}. {questions[currentQuestion].question}
+            </Question>
+            {questions[currentQuestion].choices.map((option) => (
+              <Option key={option} onClick={() => handleAnswer(option)}>
+                {option}
+              </Option>
+            ))}
+            {showCertificate && (
+              <div>
+                <ExamPassed>Congratulations, you passed the exam!</ExamPassed>
+                <DownloadCertificate>
+                  Download your certificate:
+                </DownloadCertificate>
+                <a
+                  href="https://images.besttemplates.com/3698/Custom-made-Certificate-Design-for-Completion-of-Course.jpg"
+                  download
+                >
+                  <Certificate
+                    src="https://images.besttemplates.com/3698/Custom-made-Certificate-Design-for-Completion-of-Course.jpg"
+                    alt="Certificate"
+                  />
+                </a>
+              </div>
+            )}
+            <FailedMessage>{showFailed}</FailedMessage>
+            <StartExam>
+              <LinkItem to="/lms-available">Back</LinkItem>
+            </StartExam>
+          </ExamContainer>
+        )}
+      </CourseContentContainer>
+    </React.Fragment>
+  );
+};
+
+export default SocialMediaMarketing;
